7a77e1c36c5fe22df524ff30eb36d4f38ab5cd64
DaoQuocViet18 <daoquocviet18062003@gmail.com>
2025-02-20 16:49:47 +0700

    Tilemap, MainMenuUI

---
+++ Assets/_Modules/UI/MainMenu/MainMenuUI.cs
@@ -0,0 +1,20 @@
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MainMenuUI : MonoBehaviour
+{
+    [SerializeField] private Button playButton;
+    [SerializeField] private Button exitButton;
+
+    private void Start()
+    {
+        playButton.onClick.AddListener(() => {
+            //Loader.Instance.LoadWithFade(SceneName.SelectLevelScene);
+            Loader.Instance.LoadWithFade(SceneName.GameScene);
+        });
+        exitButton.onClick.AddListener(() => {
+            Application.Quit();
+        });
+    }
+}
